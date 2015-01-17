d6179760876eaba30351f0ef0d7b0223d7a0de85
timstar3000 <tim@tattsy.com>
2015-01-18 10:31:28 +1100

    Update

--- classes.php
+++ classes.php
@@ -1,8 +1,8 @@
 <?php
 class SpotifyAPI {
-	$this->url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=itstorin&api_key=4086b09d300efcbda3db72a022559a49&format=json&period=7day&limit=3';
-	$this->json = file_get_contents($this->url);
-	$this->spotify = json_decode($this->json);
+	$url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=itstorin&api_key=4086b09d300efcbda3db72a022559a49&format=json&period=7day&limit=3';
+	$json = file_get_contents($this->url);
+	$spotify = json_decode($this->json);
 	function nowPlaying() {
 		echo '<pre>';
 		print_r($this->spotify);
